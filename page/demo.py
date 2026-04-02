799560e8d12b6c7b56f2a9d9009f4fe7ddbeaaf2
dungdoan <doan_anh_dung@manulife.com>
2026-04-02 15:26:40 +0700

    edit source path

--- page/demo.py
+++ page/demo.py
@@ -6,9 +6,8 @@ import xlwings as xw
 import tkinter as tk
 from tkinter import filedialog
 import sys
-import os.path
-sys.path.append(str(Path(__file__).parent / "src"))
-from src import ilp2026, utils_helper, map_SI_ILP2026
+sys.path.append(str(Path(__file__).parent / "src/ilp2026"))
+import ilp2026
 from data import data_source
 
 UL = 'UL_Copay0.xlsb'
